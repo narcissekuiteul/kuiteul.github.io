1e030e5794e8b06ea48875b480fce75aaf2bf96a
narcissekuiteul <narc6kuiteul@gmail.com>
2024-05-04 16:09:45 +0100

    Last modification on 4 May 2024

--- coinbase.js
+++ coinbase.js
@@ -14,13 +14,13 @@ click.addEventListener('click', (e) => {
     if(amounts == "")
         change.innerHTML = "You must enter an amount to change";
     else if(amounts == 10000)
-    change.innerHTML = "2 * 5000 or 5 * 2000 or 10 * 1000 or 20 * 500";
+    change.innerHTML = "2 * 5000 XAF";
     else if(amounts == 5000)
-        change.innerHTML = "2 * 2000 and 1 * 1000 or 5 * 1000 or 10 * 500";
+        change.innerHTML = "2 * 2000 XAF and 1 * 1000 XAF";
     else if(amounts == 2000)
-        change.innerHTML = "2 * 1000 or 4 * 500";
+        change.innerHTML = "2 * 1000 XAF";
     else if(amounts == 1000)
-        change.innerHTML = "2 * 500";
+        change.innerHTML = "2 * 500 XAF";
     else
         change.innerHTML = "The amount you enter cannot be change";
     
